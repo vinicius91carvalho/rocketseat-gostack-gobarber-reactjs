3ec7e23dcdbf91639b26f4dfcf743cee42a71dbd
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-08-05 13:16:11 -0300

    Extract components (Input and Button) from Login page

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
     <Container>
@@ -9,9 +12,14 @@ const SignIn: React.FC = () => (
             <img src={logoImg} alt="GoBarbet" />
             <form>
                 <h1>Faça seu logon</h1>
-                <input placeholder="E-mail" />
-                <input placeholder="Senha" type="password" />
-                <button type="submit">Entrar</button>
+                <Input name="email" icon={FiMail} placeholder="E-mail" />
+                <Input
+                    name="password"
+                    icon={FiLock}
+                    placeholder="Senha"
+                    type="password"
+                />
+                <Button type="submit">Entrar</Button>
                 <a href="forgot">Esqueci minha senha</a>
             </form>
             <a href="#todo">
