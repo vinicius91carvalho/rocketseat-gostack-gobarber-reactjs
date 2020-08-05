13dda7a1d7de7b0b18c67323632901b02c11230a
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-08-05 14:25:41 -0300

    Add validation on sign up form using Yup lib

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import * as Yup from 'yup';
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
-
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
-    <Container>
-        <Background />
-        <Content>
-            <img src={logoImg} alt="GoBarbet" />
-            <form>
-                <h1>Faça seu cadastro</h1>
-                <Input name="name" icon={FiUser} placeholder="Nome" />
-                <Input name="email" icon={FiMail} placeholder="E-mail" />
-                <Input
-                    name="password"
-                    icon={FiLock}
-                    placeholder="Senha"
-                    type="password"
-                />
-                <Button type="submit">Cadastrar</Button>
-            </form>
-            <a href="#todo">
-                <FiArrowLeft />
-                Voltar para logon
-            </a>
-        </Content>
-    </Container>
-);
+const SignUp: React.FC = () => {
+    const handleSubmit = useCallback(async (data: object) => {
+        try {
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Nome obrigatório'),
+                email: Yup.string()
+                    .required('E-mail obrigatório')
+                    .email('Digite um e-mail válido'),
+                password: Yup.string().min(6, 'No mínimo 6 dígitos'),
+            });
+
+            await schema.validate(data, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }, []);
+
+    return (
+        <Container>
+            <Background />
+            <Content>
+                <img src={logoImg} alt="GoBarbet" />
+                <Form onSubmit={handleSubmit}>
+                    <h1>Faça seu cadastro</h1>
+                    <Input name="name" icon={FiUser} placeholder="Nome" />
+                    <Input name="email" icon={FiMail} placeholder="E-mail" />
+                    <Input
+                        name="password"
+                        icon={FiLock}
+                        placeholder="Senha"
+                        type="password"
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                </Form>
+                <a href="#todo">
+                    <FiArrowLeft />
+                    Voltar para logon
+                </a>
+            </Content>
+        </Container>
+    );
+};
 
 export default SignUp;
