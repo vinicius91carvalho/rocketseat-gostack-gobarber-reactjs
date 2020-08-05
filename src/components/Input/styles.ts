fd40e2984909c83b2b5377413a131605f967538c
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-08-05 17:45:47 -0300

    Add friendly messages to sign up page

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,8 +1,11 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
     isFocused: boolean;
     isFilled: boolean;
+    isErrored: boolean;
 }
 
 export const Container = styled.div<ContainerProps>`
@@ -13,6 +16,12 @@ export const Container = styled.div<ContainerProps>`
 
     border: 2px solid #232129;
     color: #666360;
+
+    ${props =>
+        props.isErrored &&
+        css`
+            border-color: #c53030;
+        `}
 
     ${props =>
         props.isFocused &&
@@ -46,5 +55,21 @@ export const Container = styled.div<ContainerProps>`
 
     svg {
         margin-right: 16px;
+    }
+`;
+
+export const Error = styled(Tooltip)`
+    height: 20px;
+    margin-left: 16px;
+    svg {
+        margin-right: 0;
+    }
+    span {
+        background: #c53030;
+        color: #fff;
+
+        &::before {
+            border-color: #c53030 transparent;
+        }
     }
 `;
