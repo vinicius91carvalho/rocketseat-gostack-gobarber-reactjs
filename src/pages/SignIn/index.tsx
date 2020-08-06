0d460c56527d06de1bf4b5b0842c913db519518f
Vinicius Carvalho <vinicius91carvalho@gmail.com>
2020-08-06 18:46:02 -0300

    Create Toast compoent and configure routes

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -3,12 +3,14 @@ import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
+import { Link, useHistory } from 'react-router-dom';
 import getValidationErrors from '../../utils/getValidationErrors';
 import logoImg from '../../assets/logo.svg';
-import { Container, Content, Background } from './styles';
+import { Container, Content, AnimationContainer, Background } from './styles';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
-import { useAuth } from '../../hooks/AuthContext';
+import { useAuth } from '../../hooks/auth';
+import { useToast } from '../../hooks/toast';
 
 interface SignInFormData {
     email: string;
@@ -17,8 +19,10 @@ interface SignInFormData {
 
 const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-
     const { signIn } = useAuth();
+    const { addToast } = useToast();
+
+    const history = useHistory();
 
     const handleSubmit = useCallback(
         async (data: SignInFormData) => {
@@ -35,40 +39,60 @@ const SignIn: React.FC = () => {
                     abortEarly: false,
                 });
 
-                signIn({
+                await signIn({
                     email: data.email,
                     password: data.password,
                 });
+
+                history.push('/dashboard');
             } catch (err) {
                 if (err instanceof Yup.ValidationError) {
                     const errors = getValidationErrors(err);
                     formRef.current?.setErrors(errors);
+                    return;
                 }
+
+                addToast({
+                    type: 'error',
+                    title: 'Erro na autenticação',
+                    description:
+                        'Ocorreu um erro ao fazer login, cheque as credenciais.',
+                });
             }
         },
-        [signIn],
+        [signIn, addToast, history],
     );
 
     return (
         <Container>
             <Content>
-                <img src={logoImg} alt="GoBarbet" />
-                <Form ref={formRef} onSubmit={handleSubmit}>
-                    <h1>Faça seu logon</h1>
-                    <Input name="email" icon={FiMail} placeholder="E-mail" />
-                    <Input
-                        name="password"
-                        icon={FiLock}
-                        placeholder="Senha"
-                        type="password"
-                    />
-                    <Button type="submit">Entrar</Button>
-                    <a href="forgot">Esqueci minha senha</a>
-                </Form>
-                <a href="#todo">
-                    <FiLogIn />
-                    Criar conta
-                </a>
+                <AnimationContainer>
+                    <img src={logoImg} alt="GoBarbet" />
+                    <Form
+                        ref={formRef}
+                        onSubmit={handleSubmit}
+                        autoComplete="off"
+                    >
+                        <h1>Faça seu logon</h1>
+                        <Input
+                            name="email"
+                            icon={FiMail}
+                            placeholder="E-mail"
+                        />
+                        <Input
+                            name="password"
+                            icon={FiLock}
+                            placeholder="Senha"
+                            type="password"
+                        />
+                        <Button type="submit">Entrar</Button>
+                        <a href="forgot">Esqueci minha senha</a>
+                    </Form>
+                    <Link to="/signup">
+                        <FiLogIn />
+                        Criar conta
+                    </Link>
+                </AnimationContainer>
             </Content>
             <Background />
         </Container>
--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { shade } from 'polished';
 import signUpBackground from '../../assets/sign-up-background.png';
 
@@ -10,11 +10,31 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     display: flex;
-    place-content: center;
     align-items: center;
+    justify-content: center;
     flex-direction: column;
     width: 100%;
     max-width: 700px;
+`;
+
+const appearFromRight = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(50px);
+    }
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }
+`;
+
+export const AnimationContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    animation: ${appearFromRight} 1s;
 
     form {
         margin: 80px 0;
@@ -40,13 +60,13 @@ export const Content = styled.div`
     > a {
         display: flex;
         align-items: center;
-        color: #f4ede8;
+        color: #ff9000;
         margin-top: 24px;
         text-decoration: none;
         transition: color 0.2s;
 
         &:hover {
-            color: ${shade(0.2, '#F4EDE8')};
+            color: ${shade(0.2, '#ff9000')};
         }
 
         svg {
